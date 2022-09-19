0d045df74e2e3e98eae5cb6b85f3728d3aad757c
louislaugier <l.laugier@protonmail.com>
2022-09-20 01:01:17 +0200

    minor changes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import Typed from 'react-typed'
 import './index.css'
 import { Heading } from '@chakra-ui/react'
+import messenger from './../../illustrations/messenger.svg'
 
 export default function Home() {
 	return (
@@ -10,9 +11,9 @@ export default function Home() {
 				<Typed strings={['Votre site web avec 5 ans de nom de domaine gratuit', 'Vote site web à haut niveau de référencement naturel', 'Votre site web clé-en-main à 999€', 'Votre site web clé-en-main livré en 72h', 'Votre site web clé-en-main avec maintenance gratuite pendant 5 ans', 'Votre site web sur-mesure à prix abordable']} typeSpeed={25} backSpeed={10} loop />
 			</Heading>
 			</div>
-			{/* <div style={{width: '50vw', padding: 75}}>
-				<img src={startBuilding} alt=''></img>
-			</div> */}
+			<div style={{width: '50vw', padding: '0 100px', display: 'flex', }}>
+				<img style={{width: 400}} src={messenger} alt=''/>
+			</div>
 		</div>
 	)
 }
